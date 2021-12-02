905ec04534c79e85d9917a76ad866621b5223bc9
ali92hm <ali@alihm.net>
2021-12-02 15:30:02 -0800

    Fix style

--- data_structure_implementation/__init__.py
+++ data_structure_implementation/__init__.py
@@ -1,6 +1,8 @@
 __title__ = "data-structure-implementation"
 __description__ = "A repository of famous data structures and algorithms implemented in python for educational purpose."
-__url__ = "https://github.com/ali92hm/data-structure-implementation/blob/master/README.md"
+__url__ = (
+    "https://github.com/ali92hm/data-structure-implementation/blob/master/README.md"
+)
 __author__ = "Ali Hajimirza"
 __email__ = "ali@alihm.net"
 __copyright__ = "Copyright 2021 Ali Hajimirza"
